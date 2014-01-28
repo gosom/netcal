8a6f765f41d1c6a9bd79a2bffc2d6acc04b1f87f
Giorgos Komninos <admin@gkomninos.com>
2014-01-28 02:03:31 +0100

    changed the way we compute the 32bit integer for ip to be more elegant :)

--- netcal/client/lamport.py
+++ netcal/client/lamport.py
@@ -19,10 +19,13 @@ class LamportClock(object):
         myadd, port = self.own_id.split(':')
         otheradd, otherport = other.own_id.split(':')
 
-        o1, o2, o3, o4 = myadd.split('.')
-        myadd_number = o1 * 256*256*256 + o2 *256*256 + o3 * 256 + o4
-        o1, o2, o3, o4 = otheradd.split('.')
-        otheradd_number = o1 * 256*256*256 + o2 *256*256 + o3 * 256 + o4
+        myadd_number = self.from_string(myadd)
+        otheradd_number = self.from_string(otheradd)
+        #o1, o2, o3, o4 = myadd.split('.')
+        #myadd_number = o1 * 256*256*256 + o2 *256*256 + o3 * 256 + o4
+
+        #o1, o2, o3, o4 = otheradd.split('.')
+        #otheradd_number = o1 * 256*256*256 + o2 *256*256 + o3 * 256 + o4
 
         if myadd_number < otheradd_number:
             return -1
@@ -36,3 +39,17 @@ class LamportClock(object):
             return 1
 
         return 0
+
+    def from_string(self, s):
+        return reduce(lambda a, b: a<< 8 | b, map(int, s.split(".")))
+
+
+if __name__ == '__main__':
+    lc = LamportClock(0, "127.0.0.0:8000")
+    lc2 = LamportClock(0, "127.0.0.0:8000")
+
+    print lc.from_string("127.0.0.1")
+    print lc.from_string("127.0.0.0")
+
+    print lc < lc2
+
